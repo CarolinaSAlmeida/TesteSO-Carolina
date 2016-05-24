597468ffe8f15decfc7c1d43c50baaffd735eca8
SalmeidaCarola <carolsilveira.a@hotmail.com>
2016-05-24 15:00:26 -0300

    Exercicio 5
    O Programa calcula uma média.

---
+++ Ex5-Carolina.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+ main ()
+{ 
+   int x,soma=0;
+   float media;
+	for(x=1;x<=20;x++)
+
+	{ 
+		soma=soma+x;
+	
+	
+	}
+	media=(float)soma/(float)x;
+printf("A media e?: %2.f\n",media);	
+}
